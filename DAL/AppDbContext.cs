3b7683979bea59d259da77b9e42682fe590ff660
Michal Majewski <60788689+mgxmajewski@users.noreply.github.com>
2026-06-11 20:19:18 +0200

    feat: add app db context

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace PJATK_APBD_Cw5_s32103.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
