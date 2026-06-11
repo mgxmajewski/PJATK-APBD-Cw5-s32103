78678db86bf51e758abd43a71edce7a8c05b90e4
Michal Majewski <60788689+mgxmajewski@users.noreply.github.com>
2026-06-11 20:53:32 +0200

    feat: add patients controller

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,27 @@
+﻿using PJATK_APBD_Cw5_s32103.Models;
+
+namespace PJATK_APBD_Cw5_s32103.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class PatientsController : ControllerBase
+{
+    private readonly HospitalDbContext _context;
+
+    public PatientsController(HospitalDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllPatients()
+    {
+        var patients = await _context.Patients.ToListAsync();
+        return Ok(patients);
+    }
+}
