09a9be1d828d30a8bb464ae0f8e6244aab88b04d
Michal Majewski <60788689+mgxmajewski@users.noreply.github.com>
2026-06-11 21:48:46 +0200

    chore: register controllers and configure ef core dbcontext
    Wire up AddControllers/MapControllers, register HospitalDbContext via AddDbContext, and read the connection string from configuration (appsettings.json). Guard scaffolded OnConfiguring with IsConfigured.

--- Models/HospitalDbContext.cs
+++ Models/HospitalDbContext.cs
@@ -30,8 +30,12 @@ public partial class HospitalDbContext : DbContext
     public virtual DbSet<Ward> Wards { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=HospitalDb;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=HospitalDb;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True;");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PJATK_APBD_Cw5_s32103.Models;
+
 namespace PJATK_APBD_Cw5_s32103;
 
 public class Program
@@ -7,6 +10,9 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<HospitalDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
         builder.Services.AddAuthorization();
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -23,6 +29,8 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
+
+        app.MapControllers();
 
         app.Run();
     }
